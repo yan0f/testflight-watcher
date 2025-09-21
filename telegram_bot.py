d46d53120d202c348ca88566a0b5b2f64767ba2e
yan0f <31446439+yan0f@users.noreply.github.com>
2025-09-21 16:31:43 +0300

    Add `uv`, refactor

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,14 +1,14 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-import testflight_watcher
-import requests
 import sys
 
+import requests
+
+import testflight_watcher
 
 CHAT_ID = ""
 BOT_TOKEN = ""
-BOT_URL = "https://api.telegram.org/bot{}/sendMessage".format(BOT_TOKEN)
+
+BOT_URL = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
 MSG_NO_FULL = "TestFlight slots for <b>{}</b> beta are now available! \
 <a href='{}'>Download now</a>"
 MSG_FULL = "<b>{}</b> beta program on TestFlight is now full"
@@ -18,11 +18,17 @@ def send_notification(tf_id, free_slots, title):
     dl_url = testflight_watcher.TESTFLIGHT_URL.format(tf_id)
     if free_slots:
         message = MSG_NO_FULL.format(title, dl_url)
-    else:
-        message = MSG_FULL.format(title)
-    requests.get(BOT_URL, params={"chat_id": CHAT_ID,
-                                  "text": message,
-                                  "parse_mode": "html",
-                                  "disable_web_page_preview": "true"})
+        print(
+            requests.get(
+                BOT_URL,
+                params={
+                    "chat_id": CHAT_ID,
+                    "text": message,
+                    "parse_mode": "html",
+                    "disable_web_page_preview": "true",
+                },
+            ),
+        )
+
 
 testflight_watcher.watch(sys.argv[-1].split(","), send_notification)
--- testflight_watcher.py
+++ testflight_watcher.py
@@ -1,35 +1,37 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+
+import re
+from collections.abc import Callable
 
 import requests
 from lxml import html
-import re
-from time import sleep
-
 
 XPATH_STATUS = '//*[@class="beta-status"]/span/text()'
-XPATH_TITLE = '/html/head/title/text()'
-TITLE_REGEX = r'Join the (.+) beta - TestFlight - Apple'
-TESTFLIGHT_URL = 'https://testflight.apple.com/join/{}'
-FULL_TEXTS = ['This beta is full.',
-              "This beta isn't accepting any new testers right now."]
+XPATH_TITLE = "/html/head/title/text()"
+TITLE_REGEX = r"Join the (.+) beta - TestFlight - Apple"
+TESTFLIGHT_URL = "https://testflight.apple.com/join/{}"
+FULL_TEXTS = [
+    "This beta is full.",
+    "This beta isn't accepting any new testers right now.",
+]
 
 
-def watch(watch_ids, callback, notify_full=True, sleep_time=900):
+def watch(
+    watch_ids: list[str],
+    callback: Callable,
+    notify_full: bool = True,
+    sleep_time: int = 900,
+) -> None:
     data = {}
-    while True:
-        for tf_id in watch_ids:
-            req = requests.get(
-                TESTFLIGHT_URL.format(tf_id),
-                headers={"Accept-Language": "en-us"})
-            page = html.fromstring(req.text)
-            free_slots = page.xpath(XPATH_STATUS)[0] not in FULL_TEXTS
-
-            if (tf_id not in data or data[tf_id] != free_slots):
-                if free_slots or notify_full:
-                    title = re.findall(
-                        TITLE_REGEX,
-                        page.xpath(XPATH_TITLE)[0])[0]
-                    callback(tf_id, free_slots, title)
-                data[tf_id] = free_slots
-        sleep(sleep_time)
+    for tf_id in watch_ids:
+        req = requests.get(
+            TESTFLIGHT_URL.format(tf_id),
+            headers={"Accept-Language": "en-us"},
+        )
+        page = html.fromstring(req.text)
+        free_slots = page.xpath(XPATH_STATUS)[0] not in FULL_TEXTS
+        if tf_id not in data or data[tf_id] != free_slots:
+            if free_slots or notify_full:
+                title = re.findall(TITLE_REGEX, page.xpath(XPATH_TITLE)[0])[0]
+                callback(tf_id, free_slots, title)
+            data[tf_id] = free_slots
